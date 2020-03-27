41af28e20700511f1131bdad354c1c1123a5a680
santiago-1999-drev <betancurm.santiago@uces.edu.co>
2020-03-27 14:20:50 -0500

    covid19

--- examenes/covid19.py
+++ examenes/covid19.py
@@ -19,34 +19,16 @@ while (_decision != 4):
                 for i  in range ( len ( lista_1 )):
                     print ( lista_1 [ i ] , lista_2 [ i ])
 
-        peso_1 = 32
-        presion_1 = (6*peso_1)
-        peso_2 = 64 
-        presion_2 = (6*peso_2)
-        peso_3 = 74
-        presion_3 = (6*peso_3)
-        peso_4 = 85
-        presion_4 = (6*peso_4)
-        peso_5 = 98
-        presion_5 = (6*peso_5)
-        peso_6 = 115
-        presion_6 = (6*peso_6)
-        peso_7 = 122
-        presion_7 = (6*peso_7)
-        peso_8 = 127
-        presion_8 = (6*peso_8)
-        peso_9 = 137
-        presion_9 = (6*peso_9)
-        peso_0 = 148
-        presion_0 = (6*peso_0)
-
-        print (presion_1,presion_2,presion_3,presion_4,presion_5,presion_6,presion_7,presion_8,presion_9,presion_0)
-       
         pesosPacientes = [32,64,74,85,98,115,122,127,137,148]
-        presionesPacientes = [presion_1, presion_2, presion_3,presion_4,presion_5,presion_6,presion_7,presion_8,presion_9,presion_0 ]
+
+        presiones=[]
+        for peso in pesosPacientes:
+            presiones.append(peso*6)
+        print(presiones)
 
 
-        mostrar_dos_listas (pesosPacientes, presionesPacientes)
+
+        mostrar_dos_listas (pesosPacientes, presiones)
 
 
     elif (_decision == 2):
@@ -72,13 +54,14 @@ while (_decision != 4):
 
     elif (_decision == 3):
         print ("la presion mas alta en la lista de nuevos {} es el {}"
-        .format(presionesPacientes, max (presionesPacientes)))
+        .format(presiones, max (presiones)))
         print ("la presion mas baja en la lista de los nuevos {} es el {}"
-        .format(presionesPacientes, min (presionesPacientes)))
-        presionesPacientes.sort(reverse=True)
-        print ("lista de presiones en orden decreciente {}".format(presionesPacientes))
+        .format(presiones, min (presiones)))
+        presiones.sort(reverse=True)
+        print ("lista de presiones en orden decreciente {}".format(presiones))
         print ("esta es la cantidad de pacientes ingresados")
         print (len(pesosPacientes))
+        print (len(pesos))
                
     else:
         print("ingrese un valor valido")
